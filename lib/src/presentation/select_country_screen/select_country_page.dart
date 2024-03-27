957f2300fca96a2d1c72e9f0dc185d529e869794
SruthiBalasubramanian23 <sruthicodignus@gmail.com>
2024-03-27 13:02:13 +0530

    initial commit

--- lib/src/presentation/select_country_screen/select_country_page.dart
+++ lib/src/presentation/select_country_screen/select_country_page.dart
@@ -1,69 +1,114 @@
 import 'package:bisa_app/src/presentation/widget/button_widget.dart';
 import 'package:bisa_app/src/utils/resources/theme.dart';
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../utils/resources/asset_resources.dart';
 
-class SelectCountryPage extends StatelessWidget {
-   SelectCountryPage({super.key});
+class SelectCountryPage extends StatefulWidget {
+  const SelectCountryPage({super.key});
 
-  final TextEditingController countryController =TextEditingController();
+  @override
+  State<SelectCountryPage> createState() => _SelectCountryPageState();
+}
 
+class _SelectCountryPageState extends State<SelectCountryPage> {
+  final TextEditingController countryController = TextEditingController();
+  String countryname = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(left: 20.w,right: 20.w),
-        child:  Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 142.h),
-              width: double.infinity,
-              child: Image(image: const AssetImage(AssetResources.appLogo),height: 39.h,width: 105.w,),
-            ),
-            SizedBox(height: 100.h,),
-            SizedBox(
-              height: 92.h,
-              child: Column(
-                children: [
-                  Text("Choose Your Country",style: AppTheme.headText,),
-                  Text("Please select your country to help us for\ngive you a better expeirence",style: AppTheme.smallHead,textAlign: TextAlign.center,)
-                ],
-              ),
-            ),
-            SizedBox(height: 60.h,),
-            SizedBox(
-              height: 59.h,
-              child: TextFormField(
-                controller: countryController,
-                cursorColor: AppTheme.textColor,
-                style: AppTheme.labelTextBlack,
-                readOnly: true,
-                decoration: InputDecoration(
-                  suffixIcon: const Icon(Icons.arrow_forward_rounded,color: AppTheme.smallText,),
-                  hintText: "Select Country",
-                  hintStyle: AppTheme.smallHead,
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10.w,vertical: 20.h),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(4.r),
-                    borderSide: const BorderSide(color: AppTheme.smallText,width: 1),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(4.r),
-                    borderSide: const BorderSide(color: AppTheme.textColor,width: 1),
-                  ),
+        body: Container(
+          padding: EdgeInsets.only(left: 20.w, right: 20.w),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 142.h),
+                width: double.infinity,
+                child: Image(
+                  image: const AssetImage(AssetResources.appLogo),
+                  height: 39.h,
+                  width: 105.w,
                 ),
               ),
-            )
-
-          ],
+              SizedBox(
+                height: 100.h,
+              ),
+              SizedBox(
+                height: 92.h,
+                child: Column(
+                  children: [
+                    Text(
+                      "Choose Your Country",
+                      style: AppTheme.headText,
+                    ),
+                    Text(
+                      "Please select your country to help us for\ngive you a better expeirence",
+                      style: AppTheme.smallHead,
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 60.h,
+              ),
+              SizedBox(
+                height: 59.h,
+                child: TextFormField(
+                  onTap: () => showCountryPicker(
+                      countryListTheme: CountryListThemeData(
+                          inputDecoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(60.r),
+                              ),
+                              prefixIcon: const Icon(Icons.search),
+                              hintText: "Search Country",
+                              hintStyle: AppTheme.smallHead),
+                          backgroundColor: Colors.white,
+                          bottomSheetHeight: double.infinity,
+                          borderRadius: BorderRadius.zero),
+                      context: context,
+                      onSelect: (Country value) {
+                        setState(() {
+                          countryname = value.name!;
+                          countryController.text = countryname;
+                        });
+                      }),
+                  controller: countryController,
+                  cursorColor: AppTheme.textColor,
+                  style: AppTheme.labelTextBlack,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    suffixIcon: const Icon(
+                      Icons.arrow_forward_rounded,
+                      color: AppTheme.smallText,
+                    ),
+                    hintText: "Select Country",
+                    hintStyle: AppTheme.smallHead,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10.w, vertical: 20.h),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.r),
+                      borderSide:
+                          const BorderSide(color: AppTheme.smallText, width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.r),
+                      borderSide:
+                          const BorderSide(color: AppTheme.textColor, width: 1),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar:Padding(
-        padding:  EdgeInsets.only(bottom: 50.h,left: 20.w,right: 20.w),
-        child: const ButtonWidget(buttonTextContent: "GO AHEAD"),
-      )
-    );
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.only(bottom: 50.h, left: 20.w, right: 20.w),
+          child: const ButtonWidget(
+            buttonTextContent: "GO AHEAD",
+          ),
+        ));
   }
 }
